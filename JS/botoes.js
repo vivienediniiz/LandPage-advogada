fe18e8fc80ccbe32c1c0c7fce576b5b35bfb8087
vivienediniiz <viviennydiniz@gmail.com>
2025-09-10 20:57:30 -0300

    Commit inicial do projeto da landing page

---
+++ JS/botoes.js
@@ -0,0 +1,25 @@
+const botao = document.getElementById("button");
+
+botao.addEventListener("mouseover", () => {
+  botao.classList.add("animar");
+});
+
+botao.addEventListener("mouseout", () => {
+  botao.classList.remove("animar");
+});
+
+const botao2 = document.getElementById("button-2");
+
+botao2.addEventListener("mouseover", () => {
+  botao2.classList.add("animar");
+});
+
+botao2.addEventListener("mouseout", () => {
+  botao2.classList.remove("animar");
+});
+
+
+
+
+
+
